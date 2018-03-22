205bb02c78b975ab88c46bab7699e65c8a0e6b9b
KizzyCode <development@kizzycode.de>
2018-03-22 18:43:26 +0100

    Completely new and extremely reduced API and much less code

--- src/chunked_body.rs
+++ src/chunked_body.rs
@@ -1,188 +1,86 @@
 use std;
 
-#[derive(Eq, PartialEq, Debug)]
-enum ReaderChunkState {
-	Header([u8; 18], usize),
-	Payload(u64),
-	Trailer([u8; 2], usize, bool),
-	Done
-}
-/// Provides an API to receive and parse chunked HTTP-bodies
-#[derive(Debug)]
-pub struct ChunkedBodyReader {
-	state: ReaderChunkState
-}
-impl ChunkedBodyReader {
-	/// Creates a new `ChunkedBodyReader`
-	pub fn new() -> Self {
-		ChunkedBodyReader{ state: ReaderChunkState::Header([0u8; 18], 0) }
-	}
-	
-	fn receive_chunk_size(&mut self, mut state: ([u8; 18], usize), connection: &mut super::ReadableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Read until we receive the line-break, update state and check result
-		if let Err(error) = connection.read_until(&mut state.0, &mut state.1, "\r\n".as_bytes(), timeout) {
-			self.state = ReaderChunkState::Header(state.0, state.1);
-			return Err(error)
-		}
-		
-		// Decode length-string
-		let decoded = if let Ok(decoded) = String::from_utf8(state.0[.. state.1 - 2].to_owned()) { decoded }
-			else { return Err(std::io::Error::from(std::io::ErrorKind::InvalidData)) };
-		
-		// Parse length-string
-		let parsed = if let Ok(parsed) = u64::from_str_radix(&decoded, 16) { parsed }
-			else { return Err(std::io::Error::from(std::io::ErrorKind::InvalidData)) };
-		
-		// Check for finalizer
-		if parsed == 0 { self.state = ReaderChunkState::Trailer([0u8; 2], 0, true) }
-			else { self.state = ReaderChunkState::Payload(parsed) }
-		Ok(())
-	}
-	
-	fn receive_chunk_payload(&mut self, mut remaining: u64, buffer: &mut[u8], buffer_pos: &mut usize, connection: &mut super::ReadableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Determine the amount of bytes to receive
-		let to_receive = if remaining > (buffer.len() - *buffer_pos) as u64 { buffer.len() - *buffer_pos }
-			else { remaining as usize };
-		
-		// Receive payload
-		let old_pos = *buffer_pos;
-		let result = connection.read(&mut buffer[.. *buffer_pos + to_receive], buffer_pos, timeout);
-		remaining -= (*buffer_pos - old_pos) as u64;
-		
-		// Update state
-		self.state = if remaining > 0 { ReaderChunkState::Payload(remaining) }
-			else { ReaderChunkState::Trailer([0u8; 2], 0, false) };
-		result
-	}
-	
-	fn receive_chunk_trailer(&mut self, mut state: ([u8; 2], usize), is_last: bool, connection: &mut super::ReadableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Read the line-break
-		if let Err(error) = connection.read_until(&mut state.0, &mut state.1, "\r\n".as_bytes(), timeout) {
-			self.state = ReaderChunkState::Trailer(state.0, state.1, is_last);
-			return Err(error)
-		}
-		
-		// Set state
-		self.state = if is_last { ReaderChunkState::Done }
-			else { ReaderChunkState::Header([0u8; 18], 0) };
-		Ok(())
-	}
-}
-impl super::ReadableBody for ChunkedBodyReader {
-	fn read(&mut self, buffer: &mut[u8], buffer_pos: &mut usize, connection: &mut super::ReadableStream, timeout: std::time::Duration) -> Result<bool, std::io::Error> {
-		let timeout_point = std::time::Instant::now() + timeout;
-		while *buffer_pos < buffer.len() {
-			match self.state {
-				ReaderChunkState::Header(buffer, pos) => self.receive_chunk_size((buffer, pos), connection, super::time_remaining(timeout_point))?,
-				ReaderChunkState::Payload(remaining) => self.receive_chunk_payload(remaining, buffer, buffer_pos, connection, super::time_remaining(timeout_point))?,
-				ReaderChunkState::Trailer(buffer, pos, is_last) => self.receive_chunk_trailer((buffer, pos), is_last, connection, super::time_remaining(timeout_point))?,
-				ReaderChunkState::Done => return Ok(true)
-			};
-		}
-		Ok(false)
-	}
-	fn size(&self) -> Option<u64> {
-		None
-	}
-}
+use super::io::{ self, WriteableBuffer };
+use super::{ Error, HttpError };
 
-
-
-#[derive(Eq, PartialEq, Debug)]
-enum WriterChunkState {
-	Waiting,
-	Header(usize, usize), // to_send, buffer_pos
-	Payload(usize),
-	Trailer([u8; 2], usize, bool),
-	Finalized
+pub struct ChunkLength {
+	len: u64
 }
-/// Provides an API to write chunked HTTP-bodies
-#[derive(Debug)]
-pub struct ChunkedBodyWriter {
-	state: WriterChunkState
-}
-impl ChunkedBodyWriter {
-	/// Creates a new `ChunkedBodyWriter`
-	pub fn new() -> Self {
-		ChunkedBodyWriter{ state: WriterChunkState::Waiting }
+impl ChunkLength {
+	/// Creates a new chunk-length
+	pub fn new(length: u64) -> Self {
+		ChunkLength{ len: length }
 	}
 	
-	fn init_chunk(&mut self, buffer: &[u8], buffer_pos: &mut usize) {
-		// Determine the amount of bytes to send and update state
-		let to_send = buffer.len() - *buffer_pos;
-		self.state = WriterChunkState::Header(to_send, 0);
-	}
-	
-	fn send_chunk_size(&mut self, mut state: (usize, usize), connection: &mut super::WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Encode length
-		let encoded = format!("{0:x}\r\n", state.0);
+	/// Parses a chunk-length-field
+	pub fn parse(data: &[u8]) -> Result<Self, Error<HttpError>> {
+		// Validate length and remove trailing b"\r\n"
+		if data.len() < 3 || &data[data.len() - 2 ..] != b"\r\n" { throw_err!(HttpError::ProtocolError, "Invalid chunk-length") }
+		let data = &data[.. data.len() - 2];
 		
-		// Try to send the encoded length
-		if let Err(error) = connection.write(encoded.as_bytes(), &mut state.1, timeout) {
-			self.state = WriterChunkState::Header(state.0, state.1);
-			return Err(error);
-		}
+		// Decode and parse length-string
+		let parsed = {
+			let decoded = try_err_from!(String::from_utf8(data.to_owned()));
+			try_err_from!(u64::from_str_radix(&decoded, 16))
+		};
 		
-		// Update state
-		self.state = if state.0 == 0 { WriterChunkState::Trailer([0xD, 0xA], 0, true) }
-			else { WriterChunkState::Payload(state.0) };
-		Ok(())
+		Ok(ChunkLength{ len: parsed })
 	}
 	
-	fn send_chunk_payload(&mut self, mut remaining: usize, buffer: &[u8], buffer_pos: &mut usize, connection: &mut super::WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Determine the amount of data to send
-		let to_send = if remaining > (buffer.len() - *buffer_pos) { buffer.len() - *buffer_pos }
-			else { remaining };
+	/// Receives a chunk-length-field into `buffer` (=> reads until either `b"\r\n"` is received or
+	/// the buffer is full which causes an error) and then parses the buffer
+	///
+	/// _Note: If this function gets interrupted (fails with a non-fatal `IoError`) you can
+	/// retry/continue reading by providing the same `input` and `buffer` again_
+	pub fn read_length(input: &mut io::Reader, buffer: &mut io::WriteableBuffer<u8>, timeout: std::time::Duration) -> Result<Self, Error<HttpError>> {
+		// Try to receive the length
+		try_err!(input.read_until(b"\r\n", buffer, timeout), HttpError::IoError);
 		
-		// Send payload
-		let old_pos = *buffer_pos;
-		let result = connection.write(&buffer[.. *buffer_pos + to_send], buffer_pos, timeout);
-		remaining -= *buffer_pos - old_pos;
-		
-		// Update state
-		self.state = if remaining > 0 { WriterChunkState::Payload(remaining) }
-			else { WriterChunkState::Trailer([0u8; 2], 0, false) };
-		result
+		// Parse the buffer
+		Ok(try_err!(ChunkLength::parse(buffer.processed())))
 	}
 	
-	fn send_chunk_trailer(&mut self, mut state: ([u8; 2], usize), is_last: bool, connection: &mut super::WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Write the line-break
-		if let Err(error) = connection.write(&mut state.0, &mut state.1, timeout) {
-			self.state = WriterChunkState::Trailer(state.0, state.1, is_last);
-			return Err(error)
-		}
+	/// Returns the serialized length of this chunk-length
+	pub fn serialized_len(&self) -> usize {
+		format!("{:X}\r\n", self.len).len()
+	}
+	
+	/// Returns `(payload_length, total_length)`
+	///
+	/// _Info: This function returns a tuple because a HTTP-chunk consists of `payload || b"\r\n"`.
+	/// This means if you don't want to fuck up your stream you should not forget to read the
+	/// trailing newline!_
+	pub fn len(&self) -> (u64, u64) {
+		(self.len, self.len + 2)
+	}
+	
+	/// Serializes the chunk-length into `buffer`
+	///
+	/// Returns the serialized length
+	pub fn serialize(&self, buffer: &mut[u8]) -> Result<usize, Error<HttpError>> {
+		// Validate buffer-size and create IO-buffer
+		if buffer.len() < self.serialized_len() { throw_err!(HttpError::ApiMisuse) }
+		let mut buffer = io::MutableBackedBuffer::new(buffer);
 		
-		// Set state
-		self.state = if is_last { WriterChunkState::Finalized }
-			else { WriterChunkState::Waiting };
-		Ok(())
+		// Serialize length
+		buffer.write(format!("{:X}\r\n", self.len).as_bytes());
+		Ok(self.serialized_len())
 	}
 	
-	fn write_chunk(&mut self, buffer: &[u8], buffer_pos: &mut usize, connection: &mut super::WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		let timeout_point = std::time::Instant::now() + timeout;
-		while self.state != WriterChunkState::Finalized {
-			match self.state {
-				WriterChunkState::Waiting => self.init_chunk(buffer, buffer_pos),
-				WriterChunkState::Header(to_send, pos) => self.send_chunk_size((to_send, pos), connection, super::time_remaining(timeout_point))?,
-				WriterChunkState::Payload(pending) => self.send_chunk_payload(pending, buffer, buffer_pos, connection, super::time_remaining(timeout_point))?,
-				WriterChunkState::Trailer(buffer, pos, is_last) => self.send_chunk_trailer((buffer, pos), is_last, connection, super::time_remaining(timeout_point))?,
-				_ => ()
-			}
-		}
-		Ok(())
-	}
-}
-impl super::WriteableBody for ChunkedBodyWriter {
-	fn write(&mut self, buffer: &[u8], buffer_pos: &mut usize, connection: &mut super::WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error> {
-		// Empty chunks are not allowed in chunked bodies (except as finalizer) so we ignore them
-		if *buffer_pos < buffer.len() { self.write_chunk(buffer, buffer_pos, connection, timeout) }
-			else if let WriterChunkState::Finalized = self.state { return Err(std::io::Error::from(std::io::ErrorKind::InvalidInput)) }
-			else { Ok(()) }
-	}
-	
-	fn finalize(&mut self, connection: &mut super::WriteableStream, timeout: std::time::Duration) -> Result<bool, std::io::Error> {
-		// Send an empty chunk as finalizer
-		if self.state != WriterChunkState::Finalized { self.write_chunk(&[], &mut 0, connection, timeout)? }
-		Ok(true)
+	/// Serializes the chunk-length into `buffer` and sends it
+	///
+	/// __Warning: The buffer must have the SAME length like the serialized chunk-length_
+	///
+	/// __Warning: If `buffer.pos()` greater than `0`, the sending will be resumed WITHOUT
+	/// serializing the chunk-length__
+	///
+	/// _Note: If this function gets interrupted (fails with a non-fatal `IoError`) you can
+	/// retry/continue writing by providing the same `output` and `buffer` again_
+	pub fn write_length<T: io::ReadableBuffer<u8> + io::WriteableBuffer<u8>>(&self, output: &mut io::Writer, buffer: &mut T, timeout: std::time::Duration) -> Result<(), Error<HttpError>> {
+		// Serialize if necessary
+		if *io::WriteableBuffer::pos(buffer) == 0 { try_err!(self.serialize(buffer.remaining())); }
+		
+		// Send the (remaining) buffer
+		Ok(try_err!(output.write_exact(buffer, timeout), HttpError::IoError))
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,29 @@
-pub mod parsers;
-pub mod request_header;
-pub mod response_header;
-pub mod legacy_body;
-pub mod sized_body;
+#[macro_use] extern crate etrace;
+extern crate io;
+
+pub mod header;
 pub mod chunked_body;
 
-pub type RequestHeader = request_header::RequestHeader;
-pub type RequestHeaderReader = request_header::RequestHeaderReader;
-pub type RequestHeaderWriter = request_header::RequestHeaderWriter;
+use etrace::Error;
+pub use header::{ Header, RequestHeaderView, ResponseHeaderView };
 
-pub type ResponseHeader = response_header::ResponseHeader;
-pub type ResponseHeaderReader = response_header::ResponseHeaderReader;
-pub type ResponseHeaderWriter = response_header::ResponseHeaderWriter;
 
-pub type LegacyBodyReader = legacy_body::LegacyBodyReader;
-pub type LegacyBodyWriter = legacy_body::LegacyBodyWriter;
 
-pub type SizedBodyReader = sized_body::SizedBodyReader;
-pub type SizedBodyWriter = sized_body::SizedBodyWriter;
-
-pub type ChunkedBodyReader = chunked_body::ChunkedBodyReader;
-pub type ChunkedBodyWriter = chunked_body::ChunkedBodyWriter;
-
-/// A readable stream (probably a TCP-stream)
-pub trait ReadableStream {
-	/// Read `buffer.len() - buffer_pos` bytes from the stream into `buffer` starting at `buffer_pos`
-	fn read(&mut self, buffer: &mut[u8], buffer_pos: &mut usize, timeout: std::time::Duration) -> Result<(), std::io::Error>;
+#[derive(Debug, Clone)]
+pub enum HttpError {
+	IoError,
+	ProtocolError,
+	Unsupported,
 	
-	/// Read up to `buffer.len() - buffer_pos` bytes from the stream into `buffer` starting at
-	/// `buffer_pos` until `pattern` is matched (pattern must be included in `buffer`)
-	fn read_until(&mut self, buffer: &mut[u8], buffer_pos: &mut usize, pattern: &[u8], timeout: std::time::Duration) -> Result<(), std::io::Error>;
+	ApiMisuse
 }
-pub trait WriteableStream {
-	/// Write `buffer.len() - buffer_pos` bytes starting at `data_pos` into the stream
-	fn write(&mut self, data: &[u8], data_pos: &mut usize, timeout: std::time::Duration) -> Result<(), std::io::Error>;
+impl From<std::string::FromUtf8Error> for HttpError {
+	fn from(_: std::string::FromUtf8Error) -> Self {
+		HttpError::ProtocolError
+	}
 }
-
-
-pub trait ReadableHeader {
-	/// Reads a HTTP-header from `connection`
-	///
-	/// On non-fatal errors like `std::io::ErrorKind::TimedOut` you can try to call `read` again to complete the operation.
-	fn read(&mut self, connection: &mut ReadableStream, timeout: std::time::Duration) -> Result<(), std::io::Error>;
-}
-pub trait WriteableHeader {
-	/// Sends the HTTP-header over `connection`
-	///
-	/// On non-fatal errors like `std::io::ErrorKind::TimedOut` you can try to call `write` again to complete the operation.
-	fn write(&mut self, connection: &mut WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error>;
-}
-
-pub trait ReadableBody {
-	/// Reads the HTTP-body from `connection` into `buffer[buffer_pos ..]`
-	///
-	/// Returns `true` if the entire body was read or `false` if there are pending bytes.
-	/// `buffer_pos` will be incremented by the amount of received bytes.
-	fn read(&mut self, buffer: &mut[u8], buffer_pos: &mut usize, connection: &mut ReadableStream, timeout: std::time::Duration) -> Result<bool, std::io::Error>;
-	
-	/// Returns the size of the body or `None` if the size is indetermined
-	fn size(&self) -> Option<u64>;
-}
-pub trait WriteableBody {
-	/// Send `buffer[buffer_pos ..]` as HTTP-body
-	///
-	/// On non-fatal errors like `std::io::ErrorKind::TimedOut` you can try to call `write` again to write more bytes.
-	/// `buffer_pos` will be incremented by the amount of sent bytes.
-	fn write(&mut self, buffer: &[u8], buffer_pos: &mut usize, connection: &mut WriteableStream, timeout: std::time::Duration) -> Result<(), std::io::Error>;
-	
-	/// Finalizes an unsized HTTP-body (e.g. chunked or legacy)
-	///
-	/// Returns `true` if the body could be finalized or `false` if there are pending bytes.
-	fn finalize(&mut self, connection: &mut WriteableStream, timeout: std::time::Duration) -> Result<bool, std::io::Error>;
-}
-
-/// Underflow-safe computation of the remaining time
-pub fn time_remaining(timeout_point: std::time::Instant) -> std::time::Duration {
-	let now = std::time::Instant::now();
-	if now > timeout_point { std::time::Duration::default() } else { timeout_point - now }
+impl From<std::num::ParseIntError> for HttpError {
+	fn from(_: std::num::ParseIntError) -> Self {
+		HttpError::ProtocolError
+	}
 }
